9501759d98fa9dfb6498a91819d480c848aa25c8
daniel_as <asencio.danielfelipe@gmail.com>
2025-11-10 23:21:41 -0500

    funciones de lo mas basicas

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
+
 clock = pygame.time.Clock()
 #si la serpiente no choca no mere y si no muere el juego sigue
 death = False
@@ -13,8 +14,13 @@ death = False
 while not death:
     #mantiene la pantalla activa
     for event in pygame.event.get():
+#si no se quiere seguir se puede cerrar
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+#actualiza los fps
     pygame.display.update()
+#la pantalla ahora es blanca
+    screen.fill('white')
+#mantiene los fps en 60
     clock.tick(60)
